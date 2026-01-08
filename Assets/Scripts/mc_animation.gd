e37391a8619ba4eae3a455a0030460b5880886ba
Nanda <>
2026-01-08 13:50:31 +0700

    Update mc_animation.gd

--- Assets/Scripts/mc_animation.gd
+++ Assets/Scripts/mc_animation.gd
@@ -18,7 +18,7 @@ func  _process(_delta):
 	else :
 		animation_player.play("idle")
 #movement animation
-	if abs(mc_controller.velocity.x) > 1 :
+	if abs(mc_controller.velocity.x) > 0.0 :
 		animation_player.play("move")
 	else :
 		animation_player.play("idle")
